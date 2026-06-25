7b6ff3d6b534513cf11058480e7e7bf518364efd
Berta González <berta.gonzalez99@gmail.com>
2026-06-25 17:41:30 +0200

    feat: Add main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
 import Gallery from "./components/Gallery";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Gallery/>
+  </StrictMode>,
+)
 
