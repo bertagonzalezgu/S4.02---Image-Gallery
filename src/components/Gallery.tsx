34cc0567ffe27160cd7288011b6ba42286dd8215
Berta González <berta.gonzalez99@gmail.com>
2026-06-26 14:11:16 +0200

    Install and config shadcn/ui and tailwind.css

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,31 +1,5 @@
 import ImageItem from "./ImageItem";
-import img1 from "../assets/img/img_1.webp";
-import img2 from "../assets/img/img_2.webp";
-import img3 from "../assets/img/img_3.webp";
-import img4 from "../assets/img/img_4.webp";
-import img5 from "../assets/img/img_5.webp";
-import img6 from "../assets/img/img_6.webp";
-
-const images = [{
-    id: 1,
-    src: img1,
-    isFeatured: true
-}, {
-    id: 2,
-    src: img2
-}, {
-    id: 3,
-    src: img3
-}, {
-    id: 4,
-    src: img4
-}, {
-    id: 5,
-    src: img5
-}, {
-    id: 6,
-    src: img6
-}];
+import { images } from "../data/images";
 
 export default function Gallery(){
     const listItems = images.map((image, index) =>
---
+++ src/data/images.ts
@@ -0,0 +1,27 @@
+import img1 from "../assets/img/img_1.webp";
+import img2 from "../assets/img/img_2.webp";
+import img3 from "../assets/img/img_3.webp";
+import img4 from "../assets/img/img_4.webp";
+import img5 from "../assets/img/img_5.webp";
+import img6 from "../assets/img/img_6.webp";
+
+export const images = [{
+    id: 1,
+    src: img1,
+    isFeatured: true
+}, {
+    id: 2,
+    src: img2
+}, {
+    id: 3,
+    src: img3
+}, {
+    id: 4,
+    src: img4
+}, {
+    id: 5,
+    src: img5
+}, {
+    id: 6,
+    src: img6
+}];
