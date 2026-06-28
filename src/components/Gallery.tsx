8ac6d59ac194cc83050f6410e7acb894a2fae4ca
Berta González <berta.gonzalez99@gmail.com>
2026-06-28 13:03:16 +0200

    feat: Add useState anf handleClick function

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,9 +1,16 @@
 import ImageItem from "./ImageItem";
 import { images } from "../data/images";
+import { useState } from "react";
 
 export default function Gallery(){
-    const listItems = images.map((image, index) =>
+    const [totalImages, setTotalImages] = useState(images)
+    const listItems = totalImages.map((image, index) =>
     <ImageItem key={image.id} id={image.id} src={image.src} isFeatured={index === 0}/>)
+
+    function handleClick(id: number){
+    setTotalImages(totalImages.filter((image) => image.id !== id));
+    }
+
     return  <main className="list-none bg-yellow-100 px-5 py-5 flex justify-center">
                 <ul className="grid grid-cols-2 gap-5 md:grid-cols-4 lg:grid-cols-5">{listItems}</ul> 
             </main>
