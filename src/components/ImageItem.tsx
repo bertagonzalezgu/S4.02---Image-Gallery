e9b22c56af057265c56d2ed3c14086d93f7392c2
Berta González <berta.gonzalez99@gmail.com>
2026-06-28 12:11:24 +0200

    feat: Add props in component

--- src/components/ImageItem.tsx
+++ src/components/ImageItem.tsx
@@ -2,12 +2,18 @@ type ImageProps = {
   id: number;       
   src: string;
   isFeatured: boolean;
+  onDelete: (id: number) => void
 };
 
-export default function ImageItem({id, src, isFeatured}: ImageProps){
+export default function ImageItem({id, src, isFeatured, onDelete}: ImageProps){
     return(
         <li key={id} className={isFeatured? "lg:col-span-2 lg:row-span-2": ""}>
             <img src={src} className="w-full h-full object-cover" alt="" />
+            <button onClick={(e) => 
+                {e.stopPropagation();
+                onDelete(id);}
+                }>X
+            </button>
         </li>
     );
 };
