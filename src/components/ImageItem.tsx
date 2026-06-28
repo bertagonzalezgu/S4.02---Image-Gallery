6df9ee5657cefe86663697d970692a14ecf1fd71
Berta González <berta.gonzalez99@gmail.com>
2026-06-28 13:59:16 +0200

    fix: Add button styles

--- src/components/ImageItem.tsx
+++ src/components/ImageItem.tsx
@@ -1,3 +1,5 @@
+import closeIcon from "../assets/icons/close-svgrepo-com.svg"
+
 type ImageProps = {
   id: number;       
   src: string;
@@ -7,12 +9,13 @@ type ImageProps = {
 
 export default function ImageItem({id, src, isFeatured, onDelete}: ImageProps){
     return(
-        <li key={id} className={isFeatured? "lg:col-span-2 lg:row-span-2": ""}>
+        <li key={id} className={`relative ${isFeatured? "lg:col-span-2 lg:row-span-2": ""}`}>
             <img src={src} className="w-full h-full object-cover" alt="" />
-            <button onClick={(e) => 
+            <button className="absolute top-2 right-2" 
+                onClick={(e) => 
                 {e.stopPropagation();
                 onDelete(id);}
-                }>X
+                }><img className="w-7 h-7 bg-white bg-opacity-50 rounded-full" src={closeIcon} alt="Cruz" aria-label="Eliminar"/>
             </button>
         </li>
     );
